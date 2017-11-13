907492918f0c47274642365589c7549e255a77dc
icnpdesign <inp@bu.edu>
2017-11-13 03:50:39 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,25 @@
 'use strict';
+
+class Question {
+  constructor(content){
+    this.content = content
+    this.save()
+  }
+
+  save(){
+    this.constructor._All.push(this);
+  }
+  static All() {
+    return this._All;
+  }
+
+  static Find(id) {
+    return this.All()[id - 1]
+  }
+}
+
+Question._All = [];
+
+let newQuestion = newQuestion("Where are you right now?")
+
+newQuestion.save();
