a4cb35d0e22358c3b52f7c2a4ba2295966312411
icnpdesign <inp@bu.edu>
2017-11-13 03:44:52 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -4,7 +4,10 @@ class Question {
   constructor(content){
     this.content = content
   }
-
+  
+  save(){
+    this.constructor._All().push(this)
+  }
   static All() {
     return this._All;
   }
